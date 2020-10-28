c88afa1a89c4dbc45bfc8aab4077d6f827e199c2
cbourcier3 <celestin.bourcier@gmail.com>
2020-10-28 12:27:03 +0100

    timer avant modif

--- TP4/www/main.js
+++ TP4/www/main.js
@@ -24,85 +24,138 @@ function add(){
     var val2 = parseInt(document.getElementById('val2').value);
     document.getElementById('addResult').textContent = val1+val2;
 }
-/*
+var id = 0;
 function newTimer(){
-
+    new minuteur(parseInt(document.getElementById('heureChoisie').value),parseInt(document.getElementById('minuteChoisie').value),parseInt(document.getElementById('secondeChoisie').value),id);
+    id = id +1;
 }
+
+function timerS(){
+    var seconde = parseInt(document.getElementById('secondeChoisie').value);
+    var minute = parseInt(document.getElementById('minuteChoisie').value);
+    if((seconde == 60)&(minute <60))
+    {
+        document.getElementById('minuteChoisie').value = minute + 1;
+        document.getElementById('secondeChoisie').value = 0;
+    }
+}
+
+function timerM(){
+    var minute = parseInt(document.getElementById('minuteChoisie').value);
+    var heure = parseInt(document.getElementById('heureChoisie').value);
+    if ((minute == 60)&(heure <23)){
+        document.getElementById('heureChoisie').value = heure + 1;
+        document.getElementById('minuteChoisie').value = 0;
+    }
+}
+
 class minuteur{
-    constructor (h,m,s){
-        var time;
+    constructor (h,m,s,id){
+        this.time;
         this.h = h;
         this.m = m;
         this.s = s;
-        hChrono = '00';
-        mChrono = '00';
-        sChrono = '00';
-    } 
-*/
-var time;
-    function timerS(){
-        var seconde = parseInt(document.getElementById('secondeChoisie').value);
-        var minute = parseInt(document.getElementById('minuteChoisie').value);
-        if((seconde == 60)&(minute <60))
-        {
-            document.getElementById('minuteChoisie').value = minute + 1;
-            document.getElementById('secondeChoisie').value = 0;
-        }
-    }
-
-    function timerM(){
-        var minute = parseInt(document.getElementById('minuteChoisie').value);
-        var heure = parseInt(document.getElementById('heureChoisie').value);
-        if ((minute == 60)&(heure <23)){
-            document.getElementById('heureChoisie').value = heure + 1;
-            document.getElementById('minuteChoisie').value = 0;
-        }
-    }
-    function goTimer(){
-        time = setInterval(timer,100);
-        console.log('goTimer');
-    }
-
-    function stopTimer(){
-        clearInterval(time);
-    }
-
-    function zeroTimer(){
-        clearInterval(time);
-        document.getElementById('seconde').textContent = 00;
-        document.getElementById('minute').textContent = 00;
-        document.getElementById('heure').textContent = 00;
-    }
-    function timer(){
-        var heure = parseInt(document.getElementById('heure').textContent);
-        var minute = parseInt(document.getElementById('minute').textContent);
-        var seconde = parseInt(document.getElementById('seconde').textContent);
-        var heureA = parseInt(document.getElementById('heureChoisie').value);
-        var minuteA = parseInt(document.getElementById('minuteChoisie').value);
-        var secondeA = parseInt(document.getElementById('secondeChoisie').value);
-        if (seconde <59)
-        {
-            if (seconde<9)
-                document.getElementById('seconde').textContent = String('0')+String(seconde +1);
-            else
-                document.getElementById('seconde').textContent = seconde +1;
-        }else if ((seconde==59)&(minute<59))
-        {
-            document.getElementById('seconde').textContent = '00';
-            if(minute <9)
-                document.getElementById('minute').textContent = String('0')+String(minute +1);
-            else document.getElementById('minute').textContent = minute +1;
+        this.id = id;
+        this.hChrono = 0;
+        this.mChrono = 0;
+        this.sChrono = 0;
+        this.newLine = document.createElement('TR');
+  
+        //this.newLine = document.createElement('TR');
+        document.body.appendChild(this.newLine);
         
-        }else if ((seconde==59)&(minute==59)&(heure<23))
-        {
-            document.getElementById('seconde').textContent = '00';
-            document.getElementById('minute').textContent = '00';
-            if(heure <9)
-                document.getElementById('heure').textContent =  String('0')+String(heure +1);
-            else document.getElementById('heure').textContent = heure +1;
-        }
-        heure = parseInt(document.getElementById('heure').textContent);
-        minute = parseInt(document.getElementById('minute').textContent);
-        seconde = parseInt(document.getElementById('seconde').textContent);
-        if ((heure==heureA)&&(minute==minuteA)&&(seconde==secondeA)) stopTimer();
+        var TD1 = document.createElement('TD');
+        this.newLine.insertAdjacentElement('afterbegin',TD1);
+
+        var h2timer = document.createElement('H2');
+        h2timer.className='timer';
+        h2timer.innerHTML = '<span id=\'heure'+id+'\'>00</span>:<span id=\'minute'+id+'\'>00</span>:<span id=\'seconde'+id+'\'>00</span>';
+        TD1.insertAdjacentElement('afterbegin',h2timer);
+
+        var TD2 = document.createElement('TD');
+        this.newLine.insertAdjacentElement('beforeend',TD2);
+
+        var buttonGo = document.createElement('button');
+        buttonGo.onclick = () => this.goTimer();
+        buttonGo.textContent = 'Go!';
+        TD2.insertAdjacentElement('beforeend',buttonGo);
+
+        var TD3 = document.createElement('TD');
+        this.newLine.insertAdjacentElement('beforeend',TD3);
+
+        var buttonStop = document.createElement('button');
+        buttonStop.onclick = () => this.stopTimer();
+        buttonStop.textContent = 'Stop!';
+        TD3.insertAdjacentElement('beforeend',buttonStop);
+
+        var TD4 = document.createElement('TD');
+        this.newLine.insertAdjacentElement('beforeend',TD4);
+
+        var buttonRaz = document.createElement('button');
+        buttonRaz.onclick = () => this.zeroTimer();
+        buttonRaz.textContent = 'RaZ';
+        TD4.insertAdjacentElement('beforeend',buttonRaz);
+    } 
+
+    setHeure(h){
+        console.log(h);
+        if (parseInt(h)<10)
+            document.getElementById('heure'+this.id).textContent =String('0')+String(parseInt(h));
+        else document.getElementById('heure'+this.id).textContent = h;
+        this.hChrono = h;
     }
+
+    setMinute(m){
+        console.log(m);
+        if (parseInt(m)<10)
+            document.getElementById('minute'+this.id).textContent =String('0')+String(parseInt(m));
+        else document.getElementById('minute'+this.id).textContent = m;    
+        this.mChrono = m;
+    }
+
+    setSeconde(s){
+        console.log(s);
+        if (parseInt(s)<10)
+            document.getElementById('seconde'+this.id).textContent =String('0')+String(parseInt(s));
+        else document.getElementById('seconde'+this.id).textContent = s; 
+        this.sChrono = s;  
+    }
+
+    goTimer(){
+        //console.log(time);
+        this.time = setInterval(this.timer,1000);
+        console.log('goTimer');
+        console.log(this.sChrono);
+    }
+
+    stopTimer(){
+        clearInterval(this.time);
+    }
+
+    zeroTimer(){
+        clearInterval(this.time);
+        this.setHeure(0);
+        this.setMinute(0);
+        this.setSeconde(0);
+    }
+    
+    timer(){
+        console.log('GOOOOO');
+        console.log(this.sChrono);
+        if (this.sChrono <59)
+        {
+            this.setSeconde(parseInt(this.sChrono)+1);
+        }else if ((this.sChrono==59)&(this.mChrono<59))
+        {
+            this.setSeconde(0);
+            this.setMinute(parseInt(this.mChrono)+1);        
+        }else if ((this.sChrono==59)&(this.mChrono==59)&(this.hChrono<23))
+        {
+            this.setSeconde(0);
+            this.setMinute(0);    
+            this.setHeure(parseInt(this.hChrono)+1);    
+            
+        }
+        if ((this.h==this.hChrono)&&(this.m==this.mChrono)&&(this.s==this.sChrono)) this.stopTimer;
+    }
+}
